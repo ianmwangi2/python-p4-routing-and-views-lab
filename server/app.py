fd6186b10b11a67fafc730d724f643e728b368eb
Ian Mwangi <ian.mwangi2@student.moringaschool.com>
2024-10-04 20:33:17 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -4,5 +4,44 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route("/print/<string:route>")
+def print_string(route):
+    print(route)
+    return route
+
+@app.route("/count/<int:number>")
+def count(number):
+    count_str = f''
+    for n in range(number):
+        count_str += f"{n}\n"
+    return count_str
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, num2, operation):
+    if operation == '+':
+        return str(num1 + num2)
+
+    elif operation == '-':
+        return str(num1 - num2)
+    
+    elif operation == '*':
+        return str(num1 * num2)
+
+    elif operation == 'div':
+        if num2 == 0:
+            return 'Error: Division by zero is undefined'
+        return str(num1 / num2)
+
+    elif operation == '%':
+        if num2 == 0:
+            return 'Error: Modulo by zero is undefined'
+        return str(num1 % num2)
+
+    return "Operation not recognized. Please use one of the following: + - * div %"
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
